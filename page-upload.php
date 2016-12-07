a28b6202e24f4657f10d0c6223d6dc5d3b204361
Jordi Armengol Estapé <me@jordiae.com>
2016-12-08 00:40:07 +0100

    typo3

--- page-upload.php
+++ page-upload.php
@@ -66,8 +66,9 @@ if ($uploadOk == 0) {
     }
 }
 
-$tttt = $('ffprobe -v error -show_entries format=duration \
-  -of default=noprint_wrappers=1:nokey=1 ' . $filer);
+$command="ffprobe -v error -show_entries format=duration \
+  -of default=noprint_wrappers=1:nokey=1 " . $filer;
+$tttt=`$command`;
 echo $tttt;
 exec('ffmpeg -i '. $ruta . $filer .' -acodec flac -ar 16000  -ac 1 ' . $ruta .$filernou);
 $filernou2 = $filernou;
