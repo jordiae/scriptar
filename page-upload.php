492f6050a601307cb72d49cefb828d11c34d0dc3
Jordi Armengol Estapé <me@jordiae.com>
2016-12-08 00:29:20 +0100

    testing duration

--- page-upload.php
+++ page-upload.php
@@ -66,7 +66,9 @@ if ($uploadOk == 0) {
     }
 }
 
-
+$tttt = ffprobe -v error -show_entries format=duration \
+  -of default=noprint_wrappers=1:nokey=1 input.mp4
+echo $tttt
 exec('ffmpeg -i '. $ruta . $filer .' -acodec flac -ar 16000  -ac 1 ' . $ruta .$filernou);
 $filernou2 = $filernou;
 $results = $speech->recognize(fopen($ruta . $filernou2, 'r'), $options);
