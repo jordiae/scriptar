2cca0d4e9edf6c5d192d450b3505514d201cd992
Jordi Armengol Estapé <me@jordiae.com>
2016-12-08 02:11:09 +0100

    test

--- page-upload.php
+++ page-upload.php
@@ -73,7 +73,8 @@ if ($uploadOk == 0) {
 $array_output = array();
 exec('ffprobe -v error -show_entries format=duration \-of default=noprint_wrappers=1:nokey=1 ' . $ruta . $filer,$array_output);
 exec('echo'); // NECESSARY (LOL)
-if (floatval($array_output)>120) {
+echo floatval($array_output);
+if (floatval($array_output)>50) {
     echo 'Should you wish to upload an audio file longer than 2 minutes, please contact sales :)\n';
 }
 else {
