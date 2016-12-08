8d9096ed95c0805ced9bfd4c1f12472598345a3c
Jordi Armengol Estapé <me@jordiae.com>
2016-12-08 01:25:51 +0100

    duration check working

--- page-upload.php
+++ page-upload.php
@@ -68,11 +68,15 @@ if ($uploadOk == 0) {
 
 $array_output = array();
 exec('ffprobe -v error -show_entries format=duration \-of default=noprint_wrappers=1:nokey=1 ' . $ruta . $filer,$array_output);
-exec('ffmpeg -i '. $ruta . $filer .' -acodec flac -ar 16000  -ac 1 ' . $ruta .$filernou);
-$filernou2 = $filernou;
-$results = $speech->recognize(fopen($ruta . $filernou2, 'r'), $options);
-var_dump($results);
-var_dump($array_output);
+if (floatval($array_output[0])>120) {
+    echo 'Should you wish to upload an audio file longer than 2 minutes, please contact sales :)\n';
+}
+else {
+    exec('ffmpeg -i '. $ruta . $filer .' -acodec flac -ar 16000  -ac 1 ' . $ruta .$filernou);
+    $filernou2 = $filernou;
+    $results = $speech->recognize(fopen($ruta . $filernou2, 'r'), $options);
+    var_dump($results);
+}
  ?>
 
 </div>
