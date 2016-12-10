3e35101c08ccbc379f8bd156fc7b1c25b19a7957
Jordi Armengol Estapé <me@jordiae.com>
2016-12-10 12:07:32 -0500

    mod marq

--- page-upload.php
+++ page-upload.php
@@ -40,35 +40,35 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+// // Check if file already exists
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 20000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+// // Check file size
+// if ($_FILES["fileToUpload"]["size"] > 20000000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
 
-// Allow certain file formats
-if($imageFileType != "mp3" && $imageFileType != "ogg" && $imageFileType != "wav" ) {
-    echo "Sorry, only mp3, ogg, & wav files are allowed.";
-    $uploadOk = 0;
-}
+// // Allow certain file formats
+// if($imageFileType != "mp3" && $imageFileType != "ogg" && $imageFileType != "wav" ) {
+//     echo "Sorry, only mp3, ogg, & wav files are allowed.";
+//     $uploadOk = 0;
+// }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
+// // Check if $uploadOk is set to 0 by an error
+// if ($uploadOk == 0) {
+//     echo "Sorry, your file was not uploaded.";
+// // if everything is ok, try to upload file
+// } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+// }
 
 $array_output = array();
 exec('ffprobe -v error -show_entries format=duration \-of default=noprint_wrappers=1:nokey=1 ' . $ruta . $filer,$array_output);
